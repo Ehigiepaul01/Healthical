7cc322b854435716baa67f8607b2b9fadbe4167b
Ehigiepaul01 <paulehigie5@gmail.com>
2023-05-12 14:54:12 +0100

    updating the codes

--- js/script.js
+++ js/script.js
@@ -7,6 +7,65 @@
 //   nav.classList.toggle("hidden");
 // });
 
-$(".slick-slider").slick({
-  autoplay: true,
+const testimonials = document.querySelector('.test-container');
+const testimonialItems = document.querySelectorAll('.testimonial');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let index = 0;
+const lastIndex = testimonialItems.length - 1;
+
+function showTestimonial() {
+  testimonialItems.forEach((testimonial) => {
+    testimonial.style.display = 'none';
+  });
+
+  testimonialItems[index].style.display = 'flex';
+}
+
+function nextTestimonial() {
+  if (index === lastIndex) {
+    index = 0;
+  } else {
+    index++;
+  }
+
+  showTestimonial();
+}
+
+function prevTestimonial() {
+  if (index === 0) {
+    index = lastIndex;
+  } else {
+    index--;
+  }
+
+  showTestimonial();
+}
+
+// show first testimonial initially
+showTestimonial();
+
+// add event listeners to buttons
+nextBtn.addEventListener('click', nextTestimonial);
+prevBtn.addEventListener('click', prevTestimonial);
+
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  head.addEventListener("click", () => {
+    console.log('Clicked');
+    console.log('question.classList.contains("active"): ', question.classList.contains("active"));
+    const response = question.querySelector(".response");
+    console.log('response.style.display: ', response.style.display);
+    question.classList.toggle("active");
+    console.log('question.classList.contains("active"): ', question.classList.contains("active"));
+    if (question.classList.contains("active")) {
+      response.style.display = "block";
+    } else {
+      response.style.display = "none";
+    }
+  });
 });
+
